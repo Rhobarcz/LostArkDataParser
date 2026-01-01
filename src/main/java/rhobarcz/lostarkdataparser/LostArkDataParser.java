2af757f75248f0dad8378f8581957d4be01a54fc
rhobarcz <>
2026-01-01 01:03:08 +0100

    Initial commit

---
+++ src/main/java/rhobarcz/lostarkdataparser/LostArkDataParser.java
@@ -0,0 +1,12 @@
+package rhobarcz.lostarkdataparser;
+
+/**
+ *
+ * @author rhobarcz
+ */
+public class LostArkDataParser {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
